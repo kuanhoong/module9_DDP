668267a7ff4dc468c2c18c2c1d479b6f5a97c61d
Kuan Hoong <kuanhoong@gmail.com>
2015-12-27 20:33:14 +0800

    updates

---
+++ ui.R
@@ -0,0 +1,19 @@
+library(shiny)
+shinyUI(pageWithSidebar(
+  headerPanel("This Shiny apps calculate your Age and Display Zodiac sign"),
+  sidebarPanel(
+    dateInput("birthday", "Please enter your birthday (YYYY-MM-DD) :"),
+    checkboxGroupInput("display", "Do you want to:",
+                       c("Display Age" = "1",
+                         "Display your Zodiac" = "2")),
+    # h3("Function:"),
+    h5("This application will calculate your age and display your zodiac sign"),
+    h5("You choose what to show by clicking any of the above checkboxes."),
+    h6("The ui.r and server.r scripts can be viewed via GitHub repository at: https://github.com/kuanhoong/Developing_Data_Product")
+  ),
+  mainPanel(
+    h5('Your Birthday is:'),
+    verbatimTextOutput("age"),
+    verbatimTextOutput("zod")
+  )
+))
